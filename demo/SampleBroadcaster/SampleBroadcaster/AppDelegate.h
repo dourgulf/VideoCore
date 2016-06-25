d4785a4a70e6d69c1c10d5286eb0e3a3880edc6e
dourgulf <dourgulf@gmail.com>
2016-06-25 14:54:16 +0800

    准备合并修正版本

---
+++ demo/SampleBroadcaster/SampleBroadcaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleBroadcaster
+//
+//  Created by jinchu darwin on 16/6/25.
+//  Copyright © 2016年 dawenhing.top. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/SampleBroadcaster/SampleBroadcaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleBroadcaster
+//
+//  Created by jinchu darwin on 16/6/25.
+//  Copyright © 2016年 dawenhing.top. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
